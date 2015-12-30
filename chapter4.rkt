538da0821dfaf4538dfb413702d42f79014aab48
Mike Ackerman <mikeackerman@gmail.com>
2015-12-30 13:14:25 -0600

    Adds chapter 4 sutffs

---
+++ chapter4.rkt
@@ -0,0 +1,131 @@
+(define add1
+  (lambda (n)
+    (+ n 1)))
+
+(define sub1
+  (lambda (n)
+    (- n 1)))
+
+(define +
+  (lambda (n m)
+    (cond
+      ((zero? m) n)
+      (else (add1 (+ n (sub1 m)))))))
+
+(define addtup
+  (lambda (tup)
+    (cond
+      ((null? tup) 0)
+      (else (+ (car tup) (addtup (cdr tup)))))))
+
+(define x
+  (lambda (n m)
+    (cond
+      ((zero? m) 0)
+      (else (+ n (x n (sub1 m)))))))
+
+(define tup+
+  (lambda (tup1 tup2)
+    (cond
+      ((null? tup1) tup2)
+      ((null? tup2) tup1)
+      (else
+        (cons (+ (car tup1) (car tup2)
+          (tup+ (cdr tup1) (cdr tup2))))))))
+
+(define >
+  (lambda (n m)
+    (cond
+      ((zero? n) #f)
+      ((zero? m) #t)
+      (else (> (sub1 n) (sub1 m))))))
+
+(define <
+  (lambda (n m)
+    (cond
+      ((zero? m) #f)
+      ((zero? n) #t)
+      (else (< (sub1 n) (sub1 m))))))
+
+(define =
+  (lambda (n m)
+    (cond
+      ((zero? m) (zero? n))
+      ((zero? n) #f)
+      (else (= (sub1 n) (sub1 m))))))
+
+; equivalent = is:
+(define =
+  (lambda (n m)
+    (cond
+      ((> n m) #f)
+      ((< n m) #f)
+      (else #t))))
+
+(define expt
+  (lambda (n m)
+    (cond
+      ((zero? m) 1)
+      (else (x n (expt n (sub1 m)))))))
+
+(define divide
+  (lambda (n m)
+    (cond
+      ((< n m) 0)
+      (else (add1 (divide (- n m) m))))))
+
+(define length
+  (lambda (lat)
+    (cond
+      ((null? lat) 0)
+      (else (add1 (length (cdr lat)))))))
+
+(define pick
+  (lambda (n lat)
+    (cond
+      ((zero? (sub1 n)) (car lat))
+      (else (pick (sub1 n) (cdr lat))))))
+
+(define rempick
+  (lambda (n lat)
+    (cond
+      ((zero? (sub1 n)) (cdr lat))
+      (else (cons (car lat)
+              (rempick (sub1 n) (cdr lat)))))))
+
+(define no-nums
+  (lambda (lat)
+    (cond
+      ((null? lat) (quote ()))
+      (else (cond
+        ((number? (car lat)) (no-nums (cdr lat)))
+        (else (cons (car lat) (no-nums (cdr lat)))))))))
+
+(define all-nums
+  (lambda (lat)
+    (cond
+      ((null? lat) (quote ()))
+      (else (cond
+        ((number? (car lat))
+          (cons (car lat) (all-nums (cdr lat))))
+        (else (all-nums (cdr lat))))))))
+
+(define eqan
+  (lambda (a1 a2)
+    (cond
+      ((and (number? a1) (number? a2)) (= a1 a2))
+      ((eq? a1 a2) #t)
+      (else #f))))
+
+(define occur
+  (lambda (a lat)
+    (cond
+      ((null? lat) 0)
+      (else
+        (cond
+          ((eq? (car lat) a) (add1 (occur a (cdr lat))))
+          (else (occur a (cdr lat))))))))
+
+(define one?
+  (lambda (n)
+    (= n 1)))         
